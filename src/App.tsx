399e6b118fdb9beec3892b642a79db87bbef14c5
daeunleeeee <lde7953@gmail.com>
2025-09-06 01:24:01 +0900

    init: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">🚀 TailwindCSS 작동 확인!</h1>
+    </div>
+  );
+}
+
+export default App;
